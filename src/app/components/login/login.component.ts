faab1d05b675454f493b4d5dae355a908f70dded
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-04-13 12:00:45 -0300

    Logia del login creada

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms"
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginUsario: FormGroup;
+  loading: boolean = false;
+
+  constructor (
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
+    this.loginUsario = this.fb.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    })
+  }
+
+  ngOnInit(): void {}
+    
+    login() {
+      const email = this.loginUsario.value.email;
+      const password = this.loginUsario.value.password;
+
+      this.loading = true;
+      this.afAuth.signInWithEmailAndPassword(email, password). then ((user) => {
+        this.router.navigate(["/dashboard"]);
+      }).catch((error) => {
+        this.loading = false;
+        console.log(error);
+      })
+    }
+
+  
 
 }

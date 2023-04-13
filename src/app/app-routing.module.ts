e6b0f12f8e42f71b11f720e4031d5f3b475249f1
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-04-13 08:04:44 -0300

    Creacion del routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
+import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "registrar-usuario", component: RegistrarUsuarioComponent },
+  { path: "verificar-correo", component: VerificarCorreoComponent },
+  { path: "recuperar-password", component: RecuperarPasswordComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "**", redirectTo: "login", pathMatch: "full" }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

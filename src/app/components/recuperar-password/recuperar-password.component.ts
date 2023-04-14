37fa7cae80feea1e0217055e7be5171230c78258
Simon <116177578+simoncherry9@users.noreply.github.com>
2023-04-14 07:53:42 -0300

    Creacion de la logica para recuperar contraseña

--- src/app/components/recuperar-password/recuperar-password.component.ts
+++ src/app/components/recuperar-password/recuperar-password.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FirebaseCodeErrorService } from 'src/app/services/firebase-code-error.service';
 
 @Component({
   selector: 'app-recuperar-password',
   templateUrl: './recuperar-password.component.html',
   styleUrls: ['./recuperar-password.component.css']
 })
-export class RecuperarPasswordComponent {
+export class RecuperarPasswordComponent implements OnInit {
+  recuperarUsuario: FormGroup;
+  loading: boolean = false;
 
-}
+  constructor(
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router,
+    private firebaseError: FirebaseCodeErrorService) {
+      this.recuperarUsuario = this.fb.group({
+      correo: ["", Validators.required]
+    })
+  }
+   
+  ngOnInit(): void {
+
+  }
+
+  recuperar() {
+    const email = this.recuperarUsuario.value.correo;
+
+    this.loading = true;
+    this.afAuth.sendPasswordResetEmail(email)
+      .then(() => {
+        this.toastr.info("Revisa tu casilla de correo donde te enviamos el enlace para cambiar tu contraseña", "Enviado")
+        this.router.navigate(["/login"]);
+      })
+     .catch((error) => {
+        this.loading = false;
+        this.toastr.error(this.firebaseError.codeError(error.code), "Error");
+    })
+  
+  }}
+
--- src/app/utils/firebase-code-error.ts
+++ src/app/utils/firebase-code-error.ts
@@ -1,6 +1,6 @@
 export enum FirebaseCodeErrorEnum {
    EmailAlreadyInUse = "auth/email-already-in-use",
-   WeakPassword = "auth/auth/weak-password",
+   WeakPassword = "auth/weak-password",
    InvalidEmail = "auth/invalid-email",
    WrongPassword = "auth/wrong-password",
    UserNotFound = "auth/user-not-found",
